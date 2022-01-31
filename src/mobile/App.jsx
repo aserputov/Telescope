cb78755b4f1856135bcf03cf726ad76001fd0144
aserputov <aserputov@myseneca.ca>
2022-01-31 13:17:58 -0500

    Native Base UI library installed

--- src/mobile/App.jsx
+++ src/mobile/App.jsx
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Telescope mobile app!</Text>
-//       <StatusBar />
-//     </View>
-//   );
-// }
 import React from 'react';
 import { NativeBaseProvider, Text, Box } from 'native-base';
 
